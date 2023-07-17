cea3ac4c672d767a53c343a32c7b9fdfc2d2539e
Michael Koeppl <michael@koeppl.dev>
2023-07-17 21:14:53 +0200

    Also import apply monitor script

--- users/inet/features/desktop/hyprland.nix
+++ users/inet/features/desktop/hyprland.nix
@@ -1,4 +1,5 @@
 { inputs, lib, config, pkgs, ... }: {
   wayland.windowManager.hyprland.enable = true;
   xdg.configFile."hypr/hyprland.conf".source = ../../.config/hypr/hyprland.conf;
+  xdg.configFile."hypr/apply_monitor_setup.sh".source = ../../.config/hypr/apply_monitor_setup.sh;
 }
