fd706aa61ad635f8492a669bf058f9331bacc31f
Michael Koeppl <michael@koeppl.dev>
2023-07-20 15:24:15 +0200

    Neovim: update configuration for Neotree

--- users/inet/.config/nvim/lua/plugins/ui.lua
+++ users/inet/.config/nvim/lua/plugins/ui.lua
@@ -27,7 +27,10 @@ return {
 		opts = {
 			filesystem = {
 				bind_to_cwd = false,
-				follow_current_file = true,
+				follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
 				filtered_items = {
 					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
 					hide_dotfiles = false,
