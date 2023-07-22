e7c3ad9525c69e8776c626b0b6740b90391c8fbd
Michael Koeppl <michael@koeppl.dev>
2023-07-23 01:03:01 +0200

    Remove sudo

--- install.sh
+++ install.sh
@@ -2,28 +2,28 @@ hostname=$1
 diskname=$2
 username=$3
 
-sudo sfdisk /dev/${diskname} < sda.sfdisk
+sfdisk /dev/${diskname} < sda.sfdisk
 
-sudo mkfs.fat -F 32 /dev/${diskname}1
-sudo fatlabel /dev/${diskname}1 NIXBOOT
-sudo mkfs.btrfs -f -L NIXPERSIST /dev/${diskname}2
-sudo cryptsetup luksFormat --label NIXCRYPT /dev/${diskname}3
-sudo cryptsetup luksOpen /dev/disk/by-label/NIXCRYPT cryptroot
-sudo mkfs.btrfs -f -L NIXROOT /dev/mapper/cryptroot
+mkfs.fat -F 32 /dev/${diskname}1
+fatlabel /dev/${diskname}1 NIXBOOT
+mkfs.btrfs -f -L NIXPERSIST /dev/${diskname}2
+cryptsetup luksFormat --label NIXCRYPT /dev/${diskname}3
+cryptsetup luksOpen /dev/disk/by-label/NIXCRYPT cryptroot
+mkfs.btrfs -f -L NIXROOT /dev/mapper/cryptroot
 
-sudo mount /dev/disk/by-label/NIXROOT /mnt
-sudo mkdir /mnt/boot
-sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot
-sudo mount /dev/disk/by-label/NIXPERSIST /mnt/persist
+mount /dev/disk/by-label/NIXROOT /mnt
+mkdir /mnt/boot
+mount /dev/disk/by-label/NIXBOOT /mnt/boot
+mount /dev/disk/by-label/NIXPERSIST /mnt/persist
 
-sudo mkdir -p /mnt/persist/etc/nixos
-sudo mkdir -p /mnt/persist/etc/users
-sudo mkdir -p /mnt/persist/etc/NetworkManager/system-connections
-sudo mkdir -p /mnt/persist/etc/ssh
-sudo mkdir -p /mnt/persist/etc/wireguard/
-sudo mkdir -p /mnt/persist/var/log
-sudo mkdir -p /mnt/persist/var/lib/bluetooth
+mkdir -p /mnt/persist/etc/nixos
+mkdir -p /mnt/persist/etc/users
+mkdir -p /mnt/persist/etc/NetworkManager/system-connections
+mkdir -p /mnt/persist/etc/ssh
+mkdir -p /mnt/persist/etc/wireguard/
+mkdir -p /mnt/persist/var/log
+mkdir -p /mnt/persist/var/lib/bluetooth
 
-sudo mkpasswd -m sha-512 > /mnt/persist/etc/users/${username}
+mkpasswd -m sha-512 > /mnt/persist/etc/users/${username}
 
-sudo nixos-install --impure --flake .#${hostname}
+nixos-install --impure --flake .#${hostname}
