d13b1bdf7df51f8a200f91a3eef4c36f950ee566
Michael Koeppl <michael@koeppl.dev>
2023-07-23 20:42:51 +0200

    Fix script

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ HOSTNAME=$1
 DISK=$2
 USERNAME=$3
 
-nix run github:nix-community/disko -- --mode disko system/disko.nix --arg disks '[ "${DISK}" ]'
+nix run github:nix-community/disko -- --mode disko system/disko.nix --arg disks "[ '${DISK}' ]"
 
-mkpasswd -m sha-512 > /mnt/persist/etc/users/${username}
+mkpasswd -m sha-512 > /mnt/persist/etc/users/${USERNAME}
 
-nixos-install --impure --flake .#${hostname}
+nixos-install --impure --flake .#${HOSTNAME}
