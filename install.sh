d326e7f5acd19df01270e64e7036c46bdcafd5da
Michael Koeppl <michael@koeppl.dev>
2023-07-24 20:13:25 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -18,20 +18,20 @@ mount -t btrfs -o subvol=@persist /dev/mapper/cryptroot /mnt/persist
 mkdir -p /mnt/nix
 mount -t btrfs -o subvol=@nix /dev/mapper/cryptroot /mnt/nix
 
-mkdir -p /mnt/nix/persist/etc/nixos
-mkdir -p /mnt/nix/persist/system
-mkdir -p /mnt/nix/persist/home
-mkdir -p /mnt/nix/persist/etc/users
-mkdir -p /mnt/nix/persist/etc/NetworkManager
-mkdir -p /mnt/nix/persist/etc/ssh
-mkdir -p /mnt/nix/persist/etc/wireguard
-mkdir -p /mnt/nix/persist/var/log
-mkdir -p /mnt/nix/persist/var/lib
+mkdir -p /mnt/persist/etc/nixos
+mkdir -p /mnt/persist/system
+mkdir -p /mnt/persist/home
+mkdir -p /mnt/persist/etc/users
+mkdir -p /mnt/persist/etc/NetworkManager
+mkdir -p /mnt/persist/etc/ssh
+mkdir -p /mnt/persist/etc/wireguard
+mkdir -p /mnt/persist/var/log
+mkdir -p /mnt/persist/var/lib
 
 echo "Enter password for user ${USERNAME}"
-mkpasswd -m sha-512 > /mnt/nix/persist/etc/users/${USERNAME}
+mkpasswd -m sha-512 > /mnt/persist/etc/users/${USERNAME}
 
 echo "Enter password for user root"
-mkpasswd -m sha-512 > /mnt/nix/persist/etc/users/root
+mkpasswd -m sha-512 > /mnt/persist/etc/users/root
 
 nixos-install --impure --flake .#${HOSTNAME}
