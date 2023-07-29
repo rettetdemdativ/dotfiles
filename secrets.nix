5b44fe9efa750c6d2b8a69ccf1c19277ae0eb092
Michael Koeppl <michael@koeppl.dev>
2023-07-29 15:33:25 +0200

    Remove unused secrets-related stuff

--- secrets.nix
+++
@@ -1,9 +0,0 @@
-let
-  inet =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqlxvPpxPoKiI2ayq2ACngcEU3pwMvY5BlNWzj5YpeA michael@koeppl.dev";
-  # users = [ inet ];
-
-  nixxps =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsyxRgommxUwWK69RwCRC1yrtfIKrHUxp8Q9/dRWkGz root@nixxps";
-  # systems = [];
-in { "secrets/inet-hashed.age".publicKeys = [ inet nixxps ]; }
