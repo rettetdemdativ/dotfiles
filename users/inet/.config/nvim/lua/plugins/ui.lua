112c542f8c31fb27d9f4ff5a0cb88f5e1dfb3bc3
Michael Koeppl <michael@koeppl.dev>
2023-10-30 21:39:12 +0100

    Change indentation guides for neovim

--- users/inet/.config/nvim/lua/plugins/ui.lua
+++ users/inet/.config/nvim/lua/plugins/ui.lua
@@ -56,7 +56,7 @@ return {
 	-- fuzzy finder
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.4",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
 			{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({hidden = true})<cr>" },
@@ -285,41 +285,36 @@ return {
 		end,
 	},
 
-	-- indent-blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			-- char = "▏",
-			char = "│",
-			filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-		},
-	},
+  --{ 
+  --  "lukas-reineke/indent-blankline.nvim", 
+  --  main = "ibl", 
+  --  opts = {
+  --    indent = { char = "|" },
+  --    --exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+  --  },
+  --},
 
 	-- mini.indentscope
 	-- active indentation guide
-	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		config = function(_, opts)
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-			require("mini.indentscope").setup(opts)
-		end,
-	},
+	--{
+	--	"echasnovski/mini.indentscope",
+	--	version = false, -- wait till new 0.7.0 release to put it back on semver
+	--	event = { "BufReadPre", "BufNewFile" },
+	--	opts = {
+	--		-- symbol = "▏",
+	--		symbol = "│",
+	--		options = { try_as_border = true },
+	--	},
+	--	config = function(_, opts)
+	--		vim.api.nvim_create_autocmd("FileType", {
+	--			pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	--			callback = function()
+	--				vim.b.miniindentscope_disable = true
+	--			end,
+	--		})
+	--		require("mini.indentscope").setup(opts)
+	--	end,
+	--},
 
 	-- noice.nvim
 	-- experimental notifications ui
