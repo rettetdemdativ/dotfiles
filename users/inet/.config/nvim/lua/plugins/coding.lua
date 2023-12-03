dfc59bad4561bd3488308243b8b901afa117a817
Michael Koeppl <michael@koeppl.dev>
2023-12-03 12:43:28 +0100

    Update neovim config

--- users/inet/.config/nvim/lua/plugins/coding.lua
+++ users/inet/.config/nvim/lua/plugins/coding.lua
@@ -3,12 +3,8 @@ return {
   -- Snippets
   {
     "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
+    version = "v2.*",
+    build = "make install_jsregexp",
     opts = {
       history = true,
       delete_check_events = "TextChanged",
