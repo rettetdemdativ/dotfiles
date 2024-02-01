6527bef9c8cea239f3ca138d6ce0ee1f918ef55f
Michael Koeppl <michael@koeppl.dev>
2024-02-01 12:24:03 +0100

    Change highlighting for rainbow-delimiters

--- users/inet/.config/nvim/init.lua
+++ users/inet/.config/nvim/init.lua
@@ -27,3 +27,11 @@ vim.cmd("colorscheme vscode")
 --vim.cmd("colorscheme carbonfox")
 
 --vim.cmd("colorscheme tokyonight")
+
+vim.api.nvim_set_hl(0, 'RDYellow', { fg = "#ffd602" })
+vim.api.nvim_set_hl(0, 'RDViolet', { fg = "#d66ed2" })
+vim.api.nvim_set_hl(0, 'RDBlue', { fg = "#569cd6" })
+vim.api.nvim_set_hl(0, 'RDOrange', { fg = "#d7ba7d" })
+vim.api.nvim_set_hl(0, 'RDCyan', { fg = "#4ec9b0" })
+vim.api.nvim_set_hl(0, 'RDGreen', { fg = "#6a9955" })
+vim.api.nvim_set_hl(0, 'RDRed', { fg = "#d16969" })
--- users/inet/.config/nvim/lua/plugins/ui.lua
+++ users/inet/.config/nvim/lua/plugins/ui.lua
@@ -300,19 +300,21 @@ return {
     {
         "hiphish/rainbow-delimiters.nvim",
         config = function()
+            
+
             require("rainbow-delimiters.setup").setup {
                 query = {
                     [''] = 'rainbow-delimiters',
                     lua = 'rainbow-blocks',
                 },
                 highlight = {
-                    'RainbowDelimiterYellow',
-                    'RainbowDelimiterViolet',
-                    'RainbowDelimiterBlue',
-                    'RainbowDelimiterOrange',
-                    'RainbowDelimiterCyan',
-                    'RainbowDelimiterGreen',
-                    'RainbowDelimiterRed',
+                    'RDYellow',
+                    'RDViolet',
+                    'RDBlue',
+                    --'RDOrange',
+                    --'RDCyan',
+                    --'RDGreen',
+                    --'RDRed',
                 },
             }
         end,
