a5d1473f8b60ac1d360b9a3d610472c3abcc2e15
Michael Koeppl <michael@koeppl.dev>
2024-02-23 23:46:27 +0100

    Add .NET Core dev shell

---
+++ develop/languages/dotnet/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = ".NET Core development environment";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell { packages = with pkgs; [ dotnet-sdk_8 ]; };
+      });
+    };
+}
--- users/inet/features/development/zsh.nix
+++ users/inet/features/development/zsh.nix
@@ -12,13 +12,14 @@
       monitor_mirror = "sh $HOME/.config/hypr/apply_monitor_setup.sh mirror";
       git_relief = "sh $HOME/dotfiles/scripts/git-relief.sh";
 
-      dev_rust = "$HOME/dotfiles/scripts/activate-dev.sh languages/rust";
-      dev_go = "$HOME/dotfiles/scripts/activate-dev.sh languages/go";
       dev_c = "$HOME/dotfiles/scripts/activate-dev.sh languages/c";
+      dev_dotnet = "$HOME/dotfiles/scripts/activate-dev.sh languages/dotnet";
+      dev_go = "$HOME/dotfiles/scripts/activate-dev.sh languages/go";
       dev_java = "$HOME/dotfiles/scripts/activate-dev.sh languages/java";
       dev_java11 = "$HOME/dotfiles/scripts/activate-dev.sh languages/java11";
       dev_nodejs = "$HOME/dotfiles/scripts/activate-dev.sh languages/nodejs";
       dev_python = "$HOME/dotfiles/scripts/activate-dev.sh languages/python3";
+      dev_rust = "$HOME/dotfiles/scripts/activate-dev.sh languages/rust";
       dev_aws = "$HOME/dotfiles/scripts/activate-dev.sh stacks/aws";
     };
 
