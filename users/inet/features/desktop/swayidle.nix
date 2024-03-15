a82945ab48a0b787b5fc9988cd825d45a7f4e4f9
Michael Koeppl <michael@koeppl.dev>
2024-03-16 00:28:45 +0100

    Update configuration for swayidle

--- users/inet/features/desktop/swayidle.nix
+++ users/inet/features/desktop/swayidle.nix
@@ -3,11 +3,11 @@
   services.swayidle = {
     enable = true;
     events = [
-      { event = "before-sleep"; command = "swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
+      { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
     ];
     timeouts = [
-      { timeout = 300; command = "swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
-      { timeout = 600; command = "niri msg action power-off-monitors"; resumeCommand = "niri msg action power-off-monitors"; }
+      { timeout = 300; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
+      { timeout = 600; command = "${pkgs.niri}/bin/niri msg action power-off-monitors"; resumeCommand = "niri msg action power-off-monitors"; }
     ];
   };
 }
