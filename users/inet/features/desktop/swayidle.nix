1e06829e66e8c27b071fe6abdea4afad778cfea0
Michael Koeppl <michael@koeppl.dev>
2024-03-20 09:24:59 +0100

    Change swaylock grace period

--- users/inet/features/desktop/swayidle.nix
+++ users/inet/features/desktop/swayidle.nix
@@ -3,10 +3,10 @@
   services.swayidle = {
     enable = true;
     events = [
-      { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
+      { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 5 --fade-in 0.2"; }
     ];
     timeouts = [
-      { timeout = 300; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 2 --fade-in 0.2"; }
+      { timeout = 300; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 5 --fade-in 0.2"; }
       { timeout = 600; command = "${pkgs.niri}/bin/niri msg action power-off-monitors"; resumeCommand = "niri msg action power-off-monitors"; }
     ];
   };
