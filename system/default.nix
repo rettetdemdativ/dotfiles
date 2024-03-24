54956c54fe1c4a618cb81de4adb484716d98f584
Michael Koeppl <michael@koeppl.dev>
2024-03-24 13:25:37 +0100

    Remove system-wide mpd config

--- system/default.nix
+++ system/default.nix
@@ -123,13 +123,6 @@
 
   services.fstrim.enable = true;
 
-  # Force mpd to run under user instead of system user
-  #services.mpd.user = "${username}";
-  #systemd.services.mpd.environment = {
-  #  # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
-  #  XDG_RUNTIME_DIR = "/run/user/1000";
-  #};
-
   virtualisation = {
     podman = {
       enable = true;
