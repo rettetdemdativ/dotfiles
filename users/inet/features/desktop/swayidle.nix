06b7cd4790a7cf4c6aa76d07ad80045f72b0891e
Michael Koeppl <michael@koeppl.dev>
2024-04-03 18:22:50 +0200

    Increase time settings for swayidle

--- users/inet/features/desktop/swayidle.nix
+++ users/inet/features/desktop/swayidle.nix
@@ -6,8 +6,8 @@
       { event = "before-sleep"; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 5 --fade-in 0.2"; }
     ];
     timeouts = [
-      { timeout = 300; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 5 --fade-in 0.2"; }
-      { timeout = 600; command = "${pkgs.niri}/bin/niri msg action power-off-monitors"; resumeCommand = "niri msg action power-off-monitors"; }
+      { timeout = 600; command = "${pkgs.swaylock-effects}/bin/swaylock -f --screenshots --clock --indicator --effect-blur 7x5 --grace 5 --fade-in 0.2"; }
+      { timeout = 900; command = "${pkgs.niri}/bin/niri msg action power-off-monitors"; resumeCommand = "niri msg action power-off-monitors"; }
     ];
   };
 }
