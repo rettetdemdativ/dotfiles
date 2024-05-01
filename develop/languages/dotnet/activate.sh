2c461bfd2ee5d5b2a871d97ccec1ffcf719b1f4e
Michael Koeppl <michael@koeppl.dev>
2024-05-01 18:25:52 +0200

    Update activation script for dotnet distrobox

--- develop/languages/dotnet/activate.sh
+++ develop/languages/dotnet/activate.sh
@@ -2,6 +2,6 @@
 
 podman build -t vscode-dotnet:latest -f $HOME/dotfiles/develop/languages/dotnet/Dockerfile $HOME/dotfiles/develop/languages/dotnet
 
-distrobox create -i vscode-dotnet:latest -n vscode-dotnet --home "$HOME/vscode"
+distrobox create -i vscode-dotnet:latest -n vscode-dotnet --home "$HOME/workspace/vscode"
 
 TERM=xterm-256color distrobox enter vscode-dotnet
