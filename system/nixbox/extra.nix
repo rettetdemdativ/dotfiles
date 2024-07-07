711121dd85e708d5fbcd5406c4096f14fb618706
Michael Koeppl <michael@koeppl.dev>
2024-07-07 21:36:35 +0200

    Remove gedit from excluded GNOME packages

--- system/nixbox/extra.nix
+++ system/nixbox/extra.nix
@@ -13,7 +13,6 @@
       cheese # webcam tool
       gnome-music
       gnome-terminal
-      gedit # text editor
       epiphany # web browser
       geary # email reader
       evince # document viewer
