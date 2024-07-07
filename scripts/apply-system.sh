0ff1f9bcc13a9e1eb43cf147faf648abdfe40a93
Michael Koeppl <michael@koeppl.dev>
2024-07-07 22:36:22 +0200

    Update apply script to handle different hosts

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pushd ~/dotfiles
-doas nixos-rebuild build --flake .# && nvd diff /run/current-system result && doas nixos-rebuild switch --flake .#
+doas nixos-rebuild build --flake .# && nvd diff /run/current-system result && doas nixos-rebuild switch --flake .#$HOST
 popd
