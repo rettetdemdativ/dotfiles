8a680e907c96cb84201d04d4b5bf6c2ff97121af
Michael Koeppl <michael@koeppl.dev>
2024-07-12 01:13:22 +0200

    Update system

--- system/nixbox/extra.nix
+++ system/nixbox/extra.nix
@@ -10,21 +10,30 @@
     desktopManager.gnome.enable = true;
   };
 
-  environment.gnome.excludePackages = (with pkgs; [ gnome-photos gnome-tour ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-    ]);
+  environment.gnome.excludePackages = (with pkgs; [
+    cheese
+    gnome-terminal
+    epiphany
+    geary
+    evince
+    totem
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    gnome-music
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
+
+  security.pam.loginLimits = [{
+    domain = "*";
+    type = "soft";
+    item = "nofile";
+    value = "16384";
+  }];
 
   programs.steam = {
     enable = true;
--- users/rettetdemdativ/default.nix
+++ users/rettetdemdativ/default.nix
@@ -56,6 +56,9 @@
       ".local/share/wineprefixes"
       ".cache/protontricks"
       ".cache/winetricks"
+
+      # Lutris
+      "Games"
     ];
   };
 
