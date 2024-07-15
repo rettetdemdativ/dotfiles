4905f15a6d52ab52f91bc7f415e9900422114651
Michael Koeppl <michael@koeppl.dev>
2024-07-15 20:47:21 +0200

    Replace doas with sudo again as doas makes problems with nixos-rebuild

--- scripts/apply-system.sh
+++ scripts/apply-system.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pushd ~/dotfiles
-nixos-rebuild build --flake .#$HOST && nvd diff /run/current-system result && doas nixos-rebuild switch --flake .#$HOST
+nixos-rebuild build --flake .#$HOST && nvd diff /run/current-system result && sudo nixos-rebuild switch --flake .#$HOST
 popd
--- system/default.nix
+++ system/default.nix
@@ -110,16 +110,16 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  security.doas.enable = true;
-  security.sudo.enable = false;
-  security.doas.extraRules = [{
-    users = [ username "wheel" ];
-    # Optional, retains environment variables while running commands 
-    # e.g. retains your NIX_PATH when applying your config
-    keepEnv = true;
-    persist =
-      true; # Optional, don't ask for the password for some time, after a successfully authentication
-  }];
+  #security.doas.enable = true;
+  security.sudo.enable = true;
+  #security.doas.extraRules = [{
+  #  users = [ username "wheel" ];
+  #  # Optional, retains environment variables while running commands 
+  #  # e.g. retains your NIX_PATH when applying your config
+  #  keepEnv = true;
+  #  persist =
+  #    true; # Optional, don't ask for the password for some time, after a successfully authentication
+  #}];
 
   security.rtkit.enable = true;
   services.pipewire = {
