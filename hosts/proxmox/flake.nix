aa28b793cc2f5fd149f1af9872799d163a877872
Michael Koeppl <michael@koeppl.dev>
2025-03-10 08:46:09 +0100

    add proxmox host and user

---
+++ hosts/proxmox/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "NixOS config for VM in Proxmox";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence.url = "github:nix-community/impermanence/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    niri.url = "github:sodiboo/niri-flake";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, disko, home-manager, nixos-hardware, ... }:
+    let
+      inherit (self) outputs;
+      stateVersion = "23.05";
+      libx = import ../../lib { inherit inputs outputs nixpkgs stateVersion; };
+    in {
+      nixosConfigurations = {
+        # sudo nixos-rebuild switch --flake .#
+        nixxps = libx.mkHost {
+          hostname = "proxmox";
+          username = "mkoeppl";
+          disk = "/dev/nvme0n1";
+        };
+      };
+    };
+}
---
+++ users/mkoeppl/default.nix
@@ -0,0 +1,158 @@
+{ inputs, config, pkgs, username, ... }: {
+  imports = [ ../common.nix
+    ./features/desktop
+    ./features/development
+    ./features/general
+    ./features/media
+    ./features/cli
+    ./features/productivity
+    ];
+  # Most stuff in common.nix
+
+  home.persistence."/persist/home/${username}" = {
+    allowOther = true;
+    directories = [
+      ".ssh"
+      ".gnupg"
+      "dotfiles"
+      "workspace"
+      "Desktop"
+      "Downloads"
+      "Pictures"
+      "Music"
+      "Videos"
+      "VMs"
+      ".local/share/nvim/lazy"
+      ".local/share/nvim/mason"
+      ".local/share/mpd"
+      ".local/share/Anki2"
+      ".config/Signal"
+      ".config/mpd"
+      ".config/VSCodium"
+      ".config/tidal-hifi"
+      ".mozilla/firefox"
+      ".librewolf"
+      ".ts3client"
+
+      ".local/share/containers"
+
+      # IntelliJ
+      ".config/JetBrains"
+      ".local/share/JetBrains"
+      ".cache/JetBrains"
+
+      # Android Studio
+      ".gradle"
+      ".config/Google/AndroidStudio2022.3"
+      ".android"
+
+      # Rust
+      ".cargo"
+      ".rustup"
+
+      # Maven, unfortunately
+      ".m2"
+
+      # Nix package maintenance
+      ".cache/nixpkgs-review"
+      ".cache/nix"
+    ];
+    files = [ ".aider.conf.yml" ];
+  };
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    nautilus
+    #networkmanagerapplet
+
+    # GTK configuration through gsettings
+    # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+    glib
+    signal-desktop
+
+    # Fonts
+    monaspace
+    jetbrains-mono
+    roboto
+    inter
+    mononoki
+    cascadia-code
+  ];
+
+  home.sessionVariables = rec {
+    EDITOR = "nvim";
+    #DOCKER_HOST = "unix:///run/user/$UID/podman/podman.sock";
+    MPD_HOST = "$HOME/.config/mpd/socket";
+    GTK_THEME = "Adwaita-dark";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  gtk = {
+    enable = true;
+
+    gtk2 = {
+      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      extraConfig = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk3 = { extraConfig = { gtk-application-prefer-dark-theme = 1; }; };
+
+    gtk4 = { extraConfig = { gtk-application-prefer-dark-theme = 1; }; };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = [ "zathura.desktop" ];
+      "image/jpeg" = [ "imv.desktop" ];
+      "image/png" = [ "imv.desktop" ];
+      "x-scheme-handler/http" = [ "librewolf.desktop" ];
+      "x-scheme-handler/https" = [ "librewolf.desktop" ];
+      "text/html" = [ "librewolf.desktop" ];
+      "application/x-extension-htm" = [ "librewolf.desktop" ];
+      "application/x-extension-html" = [ "librewolf.desktop" ];
+      "application/x-extension-shtml" = [ "librewolf.desktop" ];
+      "application/x-extension-xht" = [ "librewolf.desktop" ];
+      "application/x-extension-xhtml" = [ "librewolf.desktop" ];
+      "application/xhtml+xml" = [ "librewolf.desktop" ];
+
+      "video/webm" = [ "mpv.desktop" ];
+      "video/ogg" = [ "mpv.desktop" ];
+      "video/x-msvideo" = [ "mpv.desktop" ];
+      "video/x-flv" = [ "mpv.desktop" ];
+      "video/mp4" = [ "mpv.desktop" ];
+
+      "inode/directory" = [ "nnn.desktop" "nautilus.desktop" ];
+    };
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/inet/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = { };
+}
