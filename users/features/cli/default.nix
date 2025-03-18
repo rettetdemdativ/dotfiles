795ebc6790e0a17e44a6a4e83e265683c665786b
Michael Köppl <michael@koeppl.dev>
2025-03-18 11:42:03 +0100

    disable Nix-managed Git

--- users/features/cli/default.nix
+++ users/features/cli/default.nix
@@ -1,8 +1,8 @@
 { pkgs, username, ... }: {
   home.packages = with pkgs; [
-    git
-    git-crypt
-    gnupg
+        #git
+        #git-crypt
+        #gnupg
     pinentry-qt
 
     yazi # File browser
@@ -22,17 +22,17 @@
     enableZshIntegration = true;
   };
 
-  programs.git = {
-    enable = true;
-    userName = "Michael Koeppl";
-    userEmail = "michael@koeppl.dev";
-    extraConfig = {
-      safe.directory = [ "/home/${username}/dotfiles/.git" ];
-      gpg.format = "ssh";
-      user.signingKey = "/home/${username}/.ssh/id_ed25519.pub";
-      commit.gpgsign = true;
-    };
-  };
+    #programs.git = {
+    #  enable = true;
+    #  userName = "Michael Koeppl";
+    #  userEmail = "michael@koeppl.dev";
+    #  extraConfig = {
+    #    safe.directory = [ "/home/${username}/dotfiles/.git" ];
+    #    gpg.format = "ssh";
+    #    user.signingKey = "/home/${username}/.ssh/id_ed25519.pub";
+    #    commit.gpgsign = true;
+    #  };
+    #};
 
   programs.btop = {
     enable = true;
