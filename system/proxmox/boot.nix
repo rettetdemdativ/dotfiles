3929d055c957ceaff88109b92eba527a908b4729
Michael Köppl <michael@koeppl.dev>
2025-03-24 14:32:54 +0100

    add kernel modules for Proxmox guest setup

--- system/proxmox/boot.nix
+++ system/proxmox/boot.nix
@@ -1,9 +1,9 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-  boot.initrd.availableKernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
