cf1a91dd0c4cb2e1977ba479dac648b453937115
Michael Köppl <michael@koeppl.dev>
2025-03-24 10:47:07 +0100

    add disko config without encryption

---
+++ system/disko_noluks.nix
@@ -0,0 +1,58 @@
+{ disks ? [ "/dev/vda" ], usernames, ... }: {
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              end = "-16G";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+              };
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                resumeDevice = true; # resume from hibernation from this device
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [ "size=2G" "mode=755" ];
+      };
+      "/home/${builtins.elemAt usernames 0}" = {
+        fsType = "tmpfs";
+        mountOptions = [ "size=4G" "mode=777" ];
+      };
+    };
+  };
+}
