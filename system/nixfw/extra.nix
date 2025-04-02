9b92b127fe99386b5abc4e2db23c3e5def3aad6f
Michael Koeppl <michael@koeppl.dev>
2025-04-02 17:16:58 +0200

    update TLP settings for nixfw

--- system/nixfw/extra.nix
+++ system/nixfw/extra.nix
@@ -8,7 +8,15 @@
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
-    settings = { USB_AUTOSUSPEND = 0; };
+    settings = {
+      USB_AUTOSUSPEND = 0;
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      #CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      #CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    };
   };
 
   # Instead of TLP (better support for boost)
