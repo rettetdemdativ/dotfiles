a7f4278ed352fdbd6ed55f79328d01661534ebf4
Michael Köppl <m.koeppl@proxmox.com>
2025-06-10 10:22:44 +0200

    remove persistence settings from iamb

--- users/features/general/iamb.nix
+++ users/features/general/iamb.nix
@@ -1,8 +1,3 @@
 { inputs, lib, config, pkgs, username, ... }: {
-
-  home.persistence."/persist/home/${username}" = {
-    directories = [ ".config/iamb" ".local/share/iamb" ];
-  };
-
   home.packages = with pkgs; [ iamb ];
 }
