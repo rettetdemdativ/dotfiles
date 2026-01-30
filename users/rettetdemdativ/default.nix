aeda96266871d908c1105137033a43c2fd75275a
Michael Koeppl <michael@koeppl.dev>
2026-01-30 17:57:46 +0100

    remove duplicate persistent directories
    Signed-off-by: Michael Koeppl <michael@koeppl.dev>

--- users/rettetdemdativ/default.nix
+++ users/rettetdemdativ/default.nix
@@ -13,47 +13,13 @@
       "Music"
       "Videos"
       "VMs"
-      ".local/share/nvim/lazy"
-      ".local/share/nvim/mason"
-      ".local/share/mpd"
-      ".local/share/Anki2"
-      ".config/Signal"
-      ".config/mpd"
-      ".config/VSCodium"
-      ".config/tidal-hifi"
-      ".config/libreoffice"
-      ".mozilla/firefox"
-      ".librewolf"
-      ".ts3client"
-
-      ".local/share/containers"
-
-      # IntelliJ
-      ".config/JetBrains"
-      ".local/share/JetBrains"
-      ".cache/JetBrains"
-
-      # Android Studio
-      ".gradle"
-      ".config/Google/AndroidStudio2022.3"
-      ".android"
-
-      # Rust
-      ".cargo"
-      ".rustup"
-
-      # Maven, unfortunately
-      ".m2"
 
       # Nix package maintenance
       ".cache/nixpkgs-review"
       ".cache/nix"
 
       # Steam
-      {
-        directory = ".local/share/Steam";
-        method = "symlink";
-      }
+      ".local/share/Steam"
       ".local/share/lutris"
       ".local/share/wineprefixes"
       ".cache/protontricks"
