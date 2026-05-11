121ca5eaa7026a9cef4b50eb0fc53709875017cf
Michael Köppl <m.koeppl@proxmox.com>
2026-05-11 10:50:12 +0200

    dev_vm: update flake
    Signed-off-by: Michael Köppl <m.koeppl@proxmox.com>

--- users/features/development/neovim.nix
+++ users/features/development/neovim.nix
@@ -330,13 +330,13 @@ in {
       dap.enable = true;
 
       autoclose.enable = true;
-      whitespace = {
-        enable = true;
-        settings = {
-          ignored_filetypes =
-            [ "TelescopePrompt" "Trouble" "checkhealth" "fzf" ];
-        };
-      };
+      #whitespace = {
+      #  enable = true;
+      #  settings = {
+      #    ignored_filetypes =
+      #      [ "TelescopePrompt" "Trouble" "checkhealth" "fzf" ];
+      #  };
+      #};
 
       rustaceanvim = {
         enable = true;
